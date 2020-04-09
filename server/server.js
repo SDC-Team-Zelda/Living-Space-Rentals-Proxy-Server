e5bf61ec8ad6b0c342cebacaf5aa351d347fdb8f
rlwong2 <rlwong2@gmail.com>
2020-04-09 15:55:58 -0700

    Added test for artillery

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const port = 3000;
+const morgan = require('morgan');
 
 let app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../public'));
 app.listen(port);
