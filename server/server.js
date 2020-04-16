dce1c94ead507a6ea358a147f9a73368d10d8ac4
rlwong2 <rlwong2@gmail.com>
2020-04-15 17:19:49 -0700

    Proxy properly loads service from EC2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('newrelic');
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
 const morgan = require('morgan');
 
 let app = express();
@@ -9,4 +8,11 @@ let app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../public'));
-app.listen(port);
+
+app.get('/status', (req, res) => res.send({status: "I'm alive!"}));
+
+const port = 3000;
+
+app.listen(port, function() {
+  console.log(`Proxy listening on port ${port}`);
+});
