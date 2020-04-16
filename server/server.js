a1fe430e124d806d150a678e3a0901ec3ba761fe
rlwong2 <rlwong2@gmail.com>
2020-04-15 19:52:19 -0700

    Added loader.io

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.use(express.static(__dirname + '/../public'));
 
 app.get('/status', (req, res) => res.send({status: "I'm alive!"}));
 
+app.get('/loaderio-194aceb21692d7943adc082c29a5a5de.txt', cors(), function (req, res) {
+  res.sendFile(path.join(__dirname, './public/loaderio-194aceb21692d7943adc082c29a5a5de.txt'))
+});
+
 const port = 3000;
 
 app.listen(port, function() {
